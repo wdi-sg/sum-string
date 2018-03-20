2161c1fb30630a99db87ed6993c29c5d79cc17fb
Chee Kean <ck@CK-Workstation.localdomain>
2018-03-20 10:03:39 +0800

    solution

---
+++ index.js
@@ -0,0 +1,29 @@
+const command = process.argv[2];
+const input = process.argv[3].split('');
+
+const charChecker = (elem) => {return (elem.charCodeAt(0) != 45 && (elem.charCodeAt(0) < 48 || elem.charCodeAt(0) > 57))};
+const charReducer = (acc, elem) => acc + (elem.charCodeAt(0) - 96);
+let resultNum = 0, resultChar = 0;
+
+switch (command) {
+    case 'addNumbers':
+        if (input.some(charChecker)) {
+            console.log('Error: characters detected');
+            return;
+        }
+        input.forEach((elem, index, arr) => {
+            if (elem == '-') {
+                arr[index+1] = -parseInt(arr[index+1]); // If there is no element there, it will remain as 0.
+            } else if (parseInt(elem) >= -9 && parseInt(elem) <= 9) {
+                resultNum += parseInt(elem);
+            }
+        })
+        console.log('Result: ' + resultNum);
+        break;
+    case 'addLetters':
+        console.log('Result: ' + input.reduce(charReducer, 0));
+        break;
+    default:
+        console.log('Invalid input');
+        break;
+}
